6866f12b1b8d5960b00e16475b90c1f9b2570c41
Louis Dutton <louis@dutton.digital>
2024-10-15 14:47:04 +0100

    waybar

--- home/default.nix
+++ home/default.nix
@@ -20,9 +20,46 @@
   };
 
   # desktop
-  programs.waybar.enable = true;
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        output = [
+          "eDP-1"
+          "HDMI-A-1"
+        ];
+        modules-left = [
+          "hyprland/workspaces"
+        ];
+        modules-center = [
+          "hyprland/window"
+        ];
+        modules-right = [
+          # "pulseaudio"
+          "bluetooth"
+          "network"
+          "clock"
+          "battery"
+        ];
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+        };
+      };
+    };
+
+  };
   programs.wofi = {
     enable = true;
   };
-  services.hyprpaper.enable = true;
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      splash = true;
+      ipc = false;
+    };
+  };
 }
