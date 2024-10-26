3d503238e889cd8e8db1be741f5ac7e297178587
Louis Dutton <louis@dutton.digital>
2024-10-26 12:58:17 +0100

    reduced border size

--- darwin/jankyborders.nix
+++ darwin/jankyborders.nix
@@ -2,10 +2,10 @@
 {
   services.jankyborders = {
     enable = true;
-    width = 5.0;
+    width = 3.0;
     order = "above";
     active_color = "0xff${config.lib.stylix.colors.base0D}";
     inactive_color = "0xff${config.lib.stylix.colors.base03}";
-    hidpi = true;
+    hidpi = false;
   };
 }
