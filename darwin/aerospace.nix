c7d1af3c48fc14489e7fb2606cf1b9eea50581c3
Louis Dutton <louis@dutton.digital>
2024-10-28 07:50:51 +0000

    fix aerospace workspace change

--- darwin/aerospace.nix
+++ darwin/aerospace.nix
@@ -21,7 +21,7 @@ in
         # };
 
         exec-on-workspace-change = [
-          "zsh"
+          "/bin/zsh"
           "-c"
           "sketchybar --trigger aerospace_workspace_change AEROSPACE_FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE AEROSPACE_PREV_WORKSPACE=$AEROSPACE_PREV_WORKSPACE"
         ];
