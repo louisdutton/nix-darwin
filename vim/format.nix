14eab735371b0ff786d229330add7864046d4592
Louis Dutton <louis@dutton.digital>
2024-11-07 20:11:15 +0000

    vim: fix lsp format fallback

--- vim/format.nix
+++ vim/format.nix
@@ -11,15 +11,14 @@
       settings = {
         notify_on_error = true;
         format_on_save = {
-          lspFallback = true;
-          timeoutMs = 500;
+          lsp_format = "fallback";
+          timeout_ms = 500;
         };
         formatters_by_ft = {
           javascript = [ "prettier" ];
           typescript = [ "prettier" ];
           json = [ "prettier" ];
           yaml = [ "prettier" ];
-          nix = [ "nixfmt" ];
         };
       };
     };
