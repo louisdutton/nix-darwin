38e195779f03a0bbb27a4999fa039db7a654c63f
Louis Dutton <louis@dutton.digital>
2024-11-14 10:01:12 +0000

    vim: remove custom formatter in favour of lsp

--- vim/format.nix
+++ vim/format.nix
@@ -11,14 +11,8 @@
       settings = {
         notify_on_error = true;
         format_on_save = {
-          lsp_format = "fallback";
+          lsp_format = "first";
           timeout_ms = 500;
-        };
-        formatters_by_ft = {
-          javascript = [ "prettier" ];
-          typescript = [ "prettier" ];
-          json = [ "prettier" ];
-          yaml = [ "prettier" ];
         };
       };
     };
