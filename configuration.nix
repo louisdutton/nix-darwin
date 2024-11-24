0b485608e058ad3e69547a19dff190774145c125
Louis Dutton <louis@dutton.digital>
2024-11-24 12:56:29 +0000

    remove term opacity

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@
     enable = true;
     image = ./wallpapers/catppuccin.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
-    opacity.terminal = 0.95;
+    opacity.terminal = 1.0;
     fonts.sizes.applications = 10;
     fonts.sizes.terminal = 14; # TODO make dynamic based on machine
     fonts.monospace = {
