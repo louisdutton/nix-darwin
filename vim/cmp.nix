ae8607a546843c6a42d82801df21cdcdd73948e6
Louis Dutton <louis@dutton.digital>
2024-11-24 19:21:05 +0000

    vim: replace nvim-cmp with blink-cmp

--- vim/cmp.nix
+++ vim/cmp.nix
@@ -7,36 +7,9 @@
       fromVscode = [ { } ];
     };
 
-    cmp = {
+    blink-cmp = {
       enable = true;
-      autoEnableSources = true;
-
-      settings = {
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-          { name = "luasnip"; }
-        ];
-
-        formatting.fields = [
-          "kind"
-          "abbr"
-          "menu"
-        ];
-
-        snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-
-        mapping = {
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-e>" = "cmp.mapping.close()";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-        };
-      };
+      settings.keymap.preset = "enter";
     };
   };
 }
