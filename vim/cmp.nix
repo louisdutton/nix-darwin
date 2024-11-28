b8b4f10efc9f09bf0f53d2c92ee395e313d5ba02
Louis Dutton <louis@dutton.digital>
2024-11-28 09:11:39 +0000

    vim: update cmp keymap

--- vim/cmp.nix
+++ vim/cmp.nix
@@ -2,7 +2,27 @@
   programs.nixvim.plugins = {
     blink-cmp = {
       enable = true;
-      settings.keymap.preset = "super-tab";
+      settings.keymap = {
+        preset = "enter";
+        "<C-k>" = [
+          "select_prev"
+          "fallback"
+        ];
+        "<C-j>" = [
+          "select_next"
+          "fallback"
+        ];
+        "<Tab>" = [
+          "select_next"
+          "snippet_forward"
+          "fallback"
+        ];
+        "<S-Tab>" = [
+          "select_prev"
+          "snippet_backward"
+          "fallback"
+        ];
+      };
     };
   };
 }
