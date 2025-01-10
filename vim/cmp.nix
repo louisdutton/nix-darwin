6f74779e4bac462d0e7d0920c4fe75b207c5f295
Louis Dutton <louis@dutton.digital>
2025-01-10 19:35:03 +0000

    vim: remap cmp

--- vim/cmp.nix
+++ vim/cmp.nix
@@ -2,26 +2,14 @@
   programs.nixvim.plugins = {
     blink-cmp = {
       enable = true;
+      settings.signature.enabled = true;
+      settings.completion.list.selection = "manual";
+      settings.sources.cmdline = [ ];
       settings.keymap = {
         preset = "enter";
-        "<C-k>" = [
-          "select_prev"
-          "fallback"
-        ];
-        "<C-j>" = [
-          "select_next"
-          "fallback"
-        ];
-        "<Tab>" = [
-          "select_next"
-          "snippet_forward"
-          "fallback"
-        ];
-        "<S-Tab>" = [
-          "select_prev"
-          "snippet_backward"
-          "fallback"
-        ];
+        cmdline = {
+          preset = "default";
+        };
       };
     };
   };
