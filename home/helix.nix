52d0216822a0bea40d2d71262cbaa32010610bdf
Louis Dutton <louis@dutton.digital>
2025-01-14 09:24:11 +0000

    helix: update binds and lsp config

--- home/helix.nix
+++ home/helix.nix
@@ -8,18 +8,41 @@
         cursor-shape.insert = "bar";
       };
       keys.normal = {
+        "K" = "goto_file_start";
+        "J" = "goto_last_line";
+        "H" = "goto_line_start";
+        "L" = "goto_line_end";
         ";" = "command_mode";
       };
     };
-  };
 
-  programs.helix.languages.language = [
-    { name = "rust"; }
-    {
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-    }
-    { name = "typescript"; }
-  ];
+    extraPackages = with pkgs; [ nixd ];
+
+    languages = {
+      language-server = {
+        biome = {
+          command = "biome";
+          args = [ "lsp-proxy" ];
+        };
+      };
+      language = [
+        {
+          name = "nix";
+          formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+          auto-format = true;
+        }
+        {
+          name = "typescript";
+          language-servers = [
+            {
+              name = "typescript-language-server";
+              except-features = [ "format" ];
+            }
+            "biome"
+          ];
+          auto-format = true;
+        }
+      ];
+    };
+  };
 }
