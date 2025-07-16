3c3f446927511abf7d318c3f0b86cbd9dad7c9a6
Louis Dutton <louis@dutton.digital>
2025-07-16 10:53:43 +0100

    removed nvim notify

--- home/nvim/lua/plugins.lua
+++ home/nvim/lua/plugins.lua
@@ -113,13 +113,47 @@ require("lazy").setup({
     "folke/noice.nvim",
     dependencies = {
       "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
     },
     config = function()
       require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
         presets = {
-          lsp_doc_border = true
-        }
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = true,
+        },
+        views = {
+          notify = {
+            backend = "mini",
+            relative = "editor",
+            align = "message-right",
+            timeout = 3000,
+            reverse = true,
+            focusable = false,
+            position = {
+              row = -2,
+              col = "100%",
+            },
+            size = {
+              width = "auto",
+              height = "auto",
+            },
+            border = {
+              style = "none",
+            },
+            win_options = {
+              winblend = 0,
+            },
+          },
+        },
       })
     end,
   },
