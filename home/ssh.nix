e08c77915a30cbb4eb5022b2f3d4696069cacdb0
Louis Dutton <louis@dutton.digital>
2025-09-06 06:32:18 +0100

    add mini ssh conf

--- home/ssh.nix
+++ home/ssh.nix
@@ -2,15 +2,20 @@
   # setup: ssh-keygen -f ~/.ssh/homelab && ssh-copy-id -i ~/.ssh/homelab 192.168.1.231
   programs.ssh = {
     enable = true;
-    extraConfig =
-      # ssh
-      ''
-        Host homelab
-          HostName 192.168.1.231
-          User root
-          IdentityFile ~/.ssh/homelab
-          Port 22
-          IdentitiesOnly yes
-      '';
+    extraConfig = ''
+      Host homelab
+        HostName 192.168.1.231
+        User root
+        IdentityFile ~/.ssh/homelab
+        Port 22
+        IdentitiesOnly yes
+
+      Host mini
+        HostName 192.168.1.157
+        User root
+        IdentityFile ~/.ssh/mini
+        Port 22
+        IdentitiesOnly yes
+    '';
   };
 }
