f41669483e944569c4e294973ff2077adbc45403
Louis Dutton <louis@dutton.digital>
2025-09-28 16:41:43 +0100

    update secret access

--- hosts/ideapad/default.nix
+++ hosts/ideapad/default.nix
@@ -3,6 +3,7 @@
     ./hardware-configuration.nix
     ../../modules/configuration.nix
     ../../modules/desktop.nix
+    ../../modules/sops.nix
   ];
 
   users.users.hollie.isNormalUser = true;
@@ -22,6 +23,9 @@
   powerManagement.cpuFreqGovernor = "performance";
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
+
+  # this machine doesn't have ssh setup
+  sops.age.keyFile = "/home/louis/.config/sops/age/keys.txt";
 
   system.stateVersion = "24.05";
 }
