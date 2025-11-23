a5b8deb452afb73e7bdfef2d409a021e26f56fdf
Louis Dutton <louis@dutton.digital>
2025-11-23 10:25:33 +0000

    flake update

--- darwin/jankyborders.nix
+++ darwin/jankyborders.nix
@@ -3,8 +3,6 @@
     enable = true;
     width = 3.0;
     order = "above";
-    active_color = "0xff${config.lib.stylix.colors.base0D}";
-    inactive_color = "0xff${config.lib.stylix.colors.base03}";
     hidpi = false;
   };
 }
