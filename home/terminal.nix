78ad6af67f396fdeb32e5afc64bdf0d8b6844d52
Louis Dutton <louis@dutton.digital>
2026-04-29 11:50:59 +0100

    term win decotration

--- home/terminal.nix
+++ home/terminal.nix
@@ -8,12 +8,6 @@
     package = pkgs.ghostty-bin;
     settings = {
       font-size = lib.mkForce "14.666666666666666";
-      window-decoration = "none";
-      window-padding-x = 10;
-      window-padding-y = 10;
-      adjust-cell-height = "-20%";
-
-      confirm-close-surface = false;
     };
   };
 }
