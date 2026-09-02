fe4bd42451aa92fe5a057dccda80368b6bbbd1ea
Louis Dutton <louis@dutton.digital>
2026-09-02 19:12:14 +0100

    fix ssh

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -9,6 +9,8 @@ in {
     knownHosts = mapAttrs (k: v: {publicKey = v;}) keys;
     extraConfig = ''
       Host theatre theatre.lan
+        # Avoid IPv6 address churn on the client breaking long-lived sessions.
+        AddressFamily inet
         # Detect a broken LAN path instead of leaving the terminal hung forever.
         ServerAliveInterval 15
         ServerAliveCountMax 3
